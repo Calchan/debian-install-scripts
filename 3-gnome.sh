d38018efa94c44a5d4397573868701bb553d7e8a
Denis Dupeyron (カルちゃん) <denis.dupeyron@gmail.com>
2021-08-30 18:12:31 -0600

    Add some more Gnome stuff

--- 3-gnome.sh
+++ 3-gnome.sh
@@ -6,6 +6,7 @@ apt install -y \
     gnome-shell \
     gnome-shell-extensions \
     nautilus \
+    pipewire-pulse libspa-0.2-bluetooth gstreamer1.0-pipewire \
     totem gstreamer1.0-vaapi \
     eog \
     gnome-screenshot \
@@ -13,7 +14,15 @@ apt install -y \
     file-roller unrar unzip
 apt purge -y ibus yelp
 apt autoremove -y
-apt install -y xfce4-terminal
+apt install -y gnome-terminal
+
+rm -f /etc/network/interfaces
+cd configs/gnome
+for file in $(find * -type f); do
+    mkdir -p "$(dirname "/${file}")"
+    cp -f "${file}" "/${file}"
+    chmod 644 "/${file}"
+done
 
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
