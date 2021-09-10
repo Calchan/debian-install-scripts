5c49aceaf9be84eef5602cea83143c6c6aa8a53a
Denis Dupeyron (カルちゃん) <denis.dupeyron@gmail.com>
2021-09-10 11:18:58 -0600

    Forgot set -e

--- 4-kubernetes.sh
+++ 4-kubernetes.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 wget -qO /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
