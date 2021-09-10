defd5598564e37fa0a8b0cd09d1ec96d8643638a
Denis Dupeyron (カルちゃん) <denis.dupeyron@gmail.com>
2021-09-10 11:14:47 -0600

    4-kubectl.sh -> 4-kubernetes.sh, add helm, kubectx

--- 4-kubectl.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-wget -qO /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt install -y kubectl
---
+++ 4-kubernetes.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+wget -qO /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+wget -qO - https://baltocdn.com/helm/signing.asc | apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" > /etc/apt/sources.list.d/helm-stable-debian.list
+
+apt update
+apt install -y kubectl kubectx helm
