a5322e60735336b8430b2609b308557d5980f385
Deis Dupeyron <denis.dupeyron@gmail.com>
2022-09-11 18:30:53 -0600

    4-finalize.sh: purge residual configs

--- 4-finalize.sh
+++ 4-finalize.sh
@@ -12,6 +12,7 @@ apt install -y ./google-chrome-stable_current_amd64.deb
 # termit and xterm are useless, we'll install xfce4-terminal below and it's much better
 apt purge -y ibus ifupdown yelp termit xterm
 apt autoremove -y
+apt purge -y $(dpkg -l | sed -n 's/^rc *\([^ ]*\).*/\1/p')
 
 # Remove the networking setup from the initial install since it may conflict with NetworkManager
 rm -f /etc/network/interfaces
